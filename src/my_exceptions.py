938324c90a224bf521e989e63f51c20498d588ac
MaxSlidzen <slidzon94@gmail.com>
2023-06-04 14:29:36 +0300

    corrected InstantiateCSVError

--- src/my_exceptions.py
+++ src/my_exceptions.py
@@ -1,8 +1,3 @@
 class InstantiateCSVError(Exception):
-    def __init__(self, *args, **kwargs):
-        self.message = [args[0]] if args else 'Файл item.csv поврежден'
-
-
-    def __str__(self):
-        return self.message
-
+    def __init__(self, message='Файл item.csv поврежден'):
+        super().__init__(message)
